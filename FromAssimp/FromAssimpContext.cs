94e91bf264b5757198beb5ae84165f5b017ab401
WarpZephyr <107563368+WarpZephyr@users.noreply.github.com>
2024-02-21 00:34:51 -0500

    Cleanup codebase a bit

--- FromAssimp/FromAssimpContext.cs
+++ FromAssimp/FromAssimpContext.cs
@@ -1,13 +1,12 @@
 ﻿using Assimp;
 using SoulsFormats;
-using System.IO;
 
 namespace FromAssimp
 {
     /// <summary>
     /// An <see cref="AssimpContext"/> wrapper for adding FromSoftware model support.
     /// </summary>
-    public class FromAssimpContext
+    public class FromAssimpContext : IDisposable
     {
         /// <summary>
         /// The underlying <see cref="AssimpContext"/>.
@@ -15,11 +14,29 @@ namespace FromAssimp
         public AssimpContext Context { get; set; }
 
         /// <summary>
+        /// Whether or not the underlying <see cref="AssimpContext"/> has been disposed.
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+
+        /// <summary>
         /// Create a new <see cref="FromAssimpContext"/>.
         /// </summary>
         public FromAssimpContext()
         {
             Context = new AssimpContext();
+        }
+
+        /// <summary>
+        /// Create a new <see cref="FromAssimpContext"/> from the given <see cref="AssimpContext"/>.
+        /// </summary>
+        public FromAssimpContext(AssimpContext context)
+        {
+            if (context.IsDisposed)
+            {
+                throw new InvalidOperationException($"{nameof(AssimpContext)} {nameof(context)} is already disposed.");
+            }
+
+            Context = context;
         }
 
         /// <summary>
@@ -38,8 +55,8 @@ namespace FromAssimp
                 return flver0.ToAssimpScene();
             else if (FLVER2.IsRead(path, out FLVER2 flver2))
                 return flver2.ToAssimpScene();
-            else
-                return Context.ImportFile(path, postProcessFlags);
+            
+            return Context.ImportFile(path, postProcessFlags);
         }
 
         /// <summary>
@@ -51,12 +68,10 @@ namespace FromAssimp
         public Scene ImportFileFromStream(Stream stream, PostProcessSteps postProcessFlags = PostProcessSteps.None)
         {
             byte[] bytes;
-            using (MemoryStream ms = new MemoryStream())
-            {
-                stream.CopyTo(ms);
-                ms.Seek(0, SeekOrigin.Begin);
-                bytes = ms.ToArray();
-            }
+            using var ms = new MemoryStream();
+            stream.CopyTo(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            bytes = ms.ToArray();
 
             if (MDL4.IsRead(bytes, out MDL4 mdl4))
                 return mdl4.ToAssimpScene();
@@ -66,8 +81,8 @@ namespace FromAssimp
                 return flver0.ToAssimpScene();
             else if (FLVER2.IsRead(bytes, out FLVER2 flver2))
                 return flver2.ToAssimpScene();
-            else
-                return Context.ImportFileFromStream(stream, postProcessFlags);
+            
+            return Context.ImportFileFromStream(stream, postProcessFlags);
         }
 
         /// <summary>
@@ -87,20 +102,24 @@ namespace FromAssimp
             else if (FLVER2.IsRead(bytes, out FLVER2 flver2))
                 return flver2.ToAssimpScene();
 
-            using (MemoryStream ms = new MemoryStream(bytes))
-            {
-                return Context.ImportFileFromStream(ms, postProcessFlags);
-            }
+            using var ms = new MemoryStream(bytes);
+            return Context.ImportFileFromStream(ms, postProcessFlags);
         }
 
-        public Scene ImportFileFromIFlver(IFlver model)
+        /// <summary>
+        /// Import a file from a <see cref="IFlver"/> model.
+        /// </summary>
+        /// <param name="model">The model to import from.</param>
+        /// <returns>A <see cref="Scene"/>.</returns>
+        /// <exception cref="NotSupportedException">The type using the <see cref="IFlver"/> interface was not supported.</exception>
+        public static Scene ImportFileFromIFlver(IFlver model)
         {
             if (model is FLVER0 flver0)
                 return flver0.ToAssimpScene();
             else if (model is FLVER2 flver2)
                 return flver2.ToAssimpScene();
-            else
-                throw new NotSupportedException($"The type {model.GetType()} using the {nameof(IFlver)} interface is not supported for {nameof(ImportFileFromIFlver)}");
+
+            throw new NotSupportedException($"The type {model.GetType()} using the {nameof(IFlver)} interface is not supported for {nameof(ImportFileFromIFlver)}");
         }
 
         /// <summary>
@@ -108,7 +127,7 @@ namespace FromAssimp
         /// </summary>
         /// <param name="model">The model to import from.</param>
         /// <returns>A <see cref="Scene"/>.</returns>
-        public Scene ImportFileFromFlver0(FLVER0 model)
+        public static Scene ImportFileFromFlver0(FLVER0 model)
         {
             return model.ToAssimpScene();
         }
@@ -118,7 +137,7 @@ namespace FromAssimp
         /// </summary>
         /// <param name="model">The model to import from.</param>
         /// <returns>A <see cref="Scene"/>.</returns>
-        public Scene ImportFileFromFlver2(FLVER2 model)
+        public static Scene ImportFileFromFlver2(FLVER2 model)
         {
             return model.ToAssimpScene();
         }
@@ -128,7 +147,7 @@ namespace FromAssimp
         /// </summary>
         /// <param name="model">The model to import from.</param>
         /// <returns>A <see cref="Scene"/>.</returns>
-        public Scene ImportFileFromMdl4(MDL4 model)
+        public static Scene ImportFileFromMdl4(MDL4 model)
         {
             return model.ToAssimpScene();
         }
@@ -138,108 +157,9 @@ namespace FromAssimp
         /// </summary>
         /// <param name="model">The model to import from.</param>
         /// <returns>A <see cref="Scene"/>.</returns>
-        public Scene ImportFileFromSmd4(SMD4 model)
+        public static Scene ImportFileFromSmd4(SMD4 model)
         {
             return model.ToAssimpScene();
-        }
-
-        /// <summary>
-        /// Import a file from a path, then export it to a path.
-        /// </summary>
-        /// <param name="inputPath">The path to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <param name="postProcessFlags">The post processing steps to take while importing, only for non-FromSoftware models for now.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileThenExport(string inputPath, string outputPath, string exportFormatId, PostProcessSteps postProcessFlags = PostProcessSteps.None)
-        {
-            return Context.ExportFile(ImportFile(inputPath, postProcessFlags), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from a <see cref="Stream"/>, then export it to a path.
-        /// </summary>
-        /// <param name="stream">The <see cref="Stream"/> to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <param name="postProcessFlags">The post processing steps to take while importing, only for non-FromSoftware models for now.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromStreamThenExport(Stream stream, string outputPath, string exportFormatId, PostProcessSteps postProcessFlags = PostProcessSteps.None)
-        {
-            return Context.ExportFile(ImportFileFromStream(stream, postProcessFlags), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from a byte array, then export it to a path.
-        /// </summary>
-        /// <param name="bytes">The byte array to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <param name="postProcessFlags">The post processing steps to take while importing, only for non-FromSoftware models for now.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromBytesThenExport(byte[] bytes, string outputPath, string exportFormatId, PostProcessSteps postProcessFlags = PostProcessSteps.None)
-        {
-            return Context.ExportFile(ImportFileFromBytes(bytes, postProcessFlags), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from a model type using the <see cref="IFlver"/> interface, then export it to a path. Only <see cref="FLVER0"/> and <see cref="FLVER2"/> are supported.
-        /// </summary>
-        /// <param name="model">The model to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromIFlverThenExport(IFlver model, string outputPath, string exportFormatId)
-        {
-            return Context.ExportFile(ImportFileFromIFlver(model), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from a <see cref="FLVER0"/> model, then export it to a path.
-        /// </summary>
-        /// <param name="model">The model to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromFlver0ThenExport(FLVER0 model, string outputPath, string exportFormatId)
-        {
-            return Context.ExportFile(ImportFileFromFlver0(model), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from a <see cref="FLVER2"/> model, then export it to a path.
-        /// </summary>
-        /// <param name="model">The model to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromFlver2ThenExport(FLVER2 model, string outputPath, string exportFormatId)
-        {
-            return Context.ExportFile(ImportFileFromFlver2(model), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from an <see cref="MDL4"/> model, then export it to a path.
-        /// </summary>
-        /// <param name="model">The model to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromMdl4ThenExport(MDL4 model, string outputPath, string exportFormatId)
-        {
-            return Context.ExportFile(ImportFileFromMdl4(model), outputPath, exportFormatId);
-        }
-
-        /// <summary>
-        /// Import a file from an <see cref="SMD4"/> model, then export it to a path.
-        /// </summary>
-        /// <param name="model">The model to import from.</param>
-        /// <param name="outputPath">The path to export to.</param>
-        /// <param name="exportFormatId">The format to write the exported file in.</param>
-        /// <returns>Whether or not exporting was successful.</returns>
-        public bool ImportFileFromSmd4ThenExport(SMD4 model, string outputPath, string exportFormatId)
-        {
-            return Context.ExportFile(ImportFileFromSmd4(model), outputPath, exportFormatId);
         }
 
         /// <summary>
@@ -254,17 +174,11 @@ namespace FromAssimp
             return Context.ExportFile(scene, path, exportFormatId);
         }
 
-        public List<string> GetSupportedExportOptions()
-        {
-            var strs = new List<string>();
-            var formats = Context.GetSupportedExportFormats();
-            foreach (var format in formats)
-            {
-                strs.Add(format.FormatId);
-            }
-            return strs;
-        }
-
+        /// <summary>
+        /// Get the appropiate extension for the chosen format.
+        /// </summary>
+        /// <param name="format">The format to get the extension of.</param>
+        /// <returns>The extension of the given format.</returns>
         public static string GetFormatExtension(string format)
         {
             return format switch
@@ -276,5 +190,35 @@ namespace FromAssimp
                 _ => format
             };
         }
+
+        #region IDisposable Implementation
+
+        /// <summary>
+        /// Disposes the underlying <see cref="AssimpContext"/>.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDisposed)
+            {
+                if (disposing)
+                {
+                    Context.Dispose();
+                }
+
+                IsDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Disposes the underlying <see cref="AssimpContext"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
     }
 }
